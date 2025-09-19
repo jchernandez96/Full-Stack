589d752e15605d912dcdc6233771d1c202c39b79
jchernandez96 <jc.hernandez11@ciaf.edu.co>
2025-09-18 19:25:16 -0500

    Servidores
    Ruta "/","leer",y Page no found

--- fs-server.js
+++ fs-server.js
@@ -8,16 +8,29 @@ const EventEmitter = new ev();
 
 //Server 
 const server = http.createServer((req, res)=>{
-    fs.readFile("messages.txt", "utf-8",(err,data)=>{
-        if(err){
-            //Mensaje de error por HTTP
-            res.writeHead(500, {"content-type":"text/plain"});
-            res.end("Error de lectura de archivo");
-        };
-        //Mensaje de exito por HTTP
-        res.writeHead(200,{"content-type":"text/plain"});
-        res.end(`Exito de lectura de archivo ${data}`);
-    });
+
+    const filePath = path.join(__dirname, "messages.txt");
+
+    if(req.url==="/"){
+        res.writeHead(200, {"content-type":"text/plain"});
+        res.end("Bienvenido Al Servidor De Archivos");
+    }
+    else if(req.url === "/leer"){
+            fs.readFile(filePath, "utf-8",(err,data)=>{
+            if(err){
+                //Mensaje de error por HTTP
+                res.writeHead(500, {"content-type":"text/plain"});
+                res.end("Error de lectura de archivo");
+            };
+            //Mensaje de exito por HTTP
+            res.writeHead(200,{"content-type":"text/plain"});
+            res.end(`Exito de lectura de archivo ${data}`);
+        });
+    }else{
+        //Mensaje de error por HTTP
+        res.writeHead(404, {"content-type":"text/plain"});
+        res.end("Page no found");
+    }
 });
 
 server.listen(3000, ()=>{
